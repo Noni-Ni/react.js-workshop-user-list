dc1d6352fd163c11aa333ff935d8bbb7b18d8c9f
Noni-Ni <nonini1109@gmail.com>
2024-07-13 22:36:43 -0500

    add search functionlity

--- client/src/components/user-section/UserSection.jsx
+++ client/src/components/user-section/UserSection.jsx
@@ -15,26 +15,30 @@ export default function UserSection(props) {
     const [showUserById, setShowUserById] = useState(null);
     const [showDeleteUserById, setShowDeleteUserById] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    
-    useEffect(()=>{
-        (async function getUsers(){
+    const [search, setSearch] = useState({
+        searchText: '',
+        criteria: 'Not selected'
+    });
+
+    useEffect(() => {
+        (async function getUsers() {
             try {
                 const response = await fetch(`${baseUrl}/users`);
                 const result = await response.json();
-                
+
                 const data = Object.values(result);
                 setUsers(data);
-                
+
             } catch (error) {
                 alert(error.message)
             } finally {
                 setIsLoading(false);
             }
-           
-        })();
-    }, []);
 
-    const addUserClickHandler = () =>{
+        })();
+    }, [search.searchText]);
+
+    const addUserClickHandler = () => {
         setShowAddUser(true);
     }
 
@@ -42,37 +46,65 @@ export default function UserSection(props) {
         setShowAddUser(false);
     }
 
-    const showUserDetailsClickHandler = (userId) =>{
-       
-       setShowUserById(userId)
-        
+    const showUserDetailsClickHandler = (userId) => {
+
+        setShowUserById(userId)
+
     }
 
     const userDeleteClickHandler = (userId) => {
         console.log(userId)
         setShowDeleteUserById(userId)
-        
+
     }
     const userDeleteCloseHandler = (userId) => {
         console.log(userId)
         setShowDeleteUserById(null)
-        
+
     }
 
-    const userDeleteHandler = async(userId) => {
+    const userDeleteHandler = async (userId) => {
         console.log(userId)
-        
-        const response = await fetch(`${baseUrl}/users/${userId}`, {method: 'DELETE',})
+
+        const response = await fetch(`${baseUrl}/users/${userId}`, { method: 'DELETE', })
 
         setUsers(oldUsers => oldUsers.filter(user => user._id !== userId))
 
         setShowDeleteUserById(null)
     }
 
+    const formHandler = (event) => {
+
+        event.preventDefault();
+
+        setUsers(oldUsers => oldUsers.filter(user => (user[search.criteria]).includes(search.searchText)))
+
+
+
+    }
+
+    const deleteSearch = () => {
+        setSearch({
+            searchText: '',
+            criteria: 'Not selected'
+        })
+
+        
+    }
+
+    const onChangeHandler = (event) => {
+
+        setSearch(oldValues => ({
+            ...oldValues,
+            [event.target.name]: event.target.value
+        }))
+
+    }
+
 
     const addUserSaveHandler = async (e) => {
         e.preventDefault();
-        
+
         const formData = new FormData(e.currentTarget);
         const userData = {
             ...Object.fromEntries(formData),
@@ -90,26 +122,27 @@ export default function UserSection(props) {
 
         const responseData = await response.json();
 
-        setUsers(oldUsers => [...oldUsers , responseData]);
+        setUsers(oldUsers => [...oldUsers, responseData]);
         setShowAddUser(false);
-        
+
     }
 
     return (
         <section className="card users-container">
-            <Search />
 
-            <UserList users={users} showUserDetailsClickHandler = {showUserDetailsClickHandler} userDeleteClickHandler = {userDeleteClickHandler} isLoading = {isLoading} />
+            <Search searchValue={search.searchText} searchCriteria={search.criteria} onChangeHandler={onChangeHandler} formHandler={formHandler} deleteSearch={deleteSearch} />
 
-            {showAddUser && <UserAdd onClose= {addUserCloseHandler} onSave = {addUserSaveHandler} />}
-            
-            {showUserById && <UserDetails user = {users.find(user => user._id == showUserById)} onClose={() => setShowUserById(null)}/>}
+            <UserList users={users} showUserDetailsClickHandler={showUserDetailsClickHandler} userDeleteClickHandler={userDeleteClickHandler} isLoading={isLoading} />
 
-            {showDeleteUserById && <UserDelete onClose={userDeleteCloseHandler} onUserDelete={() => userDeleteHandler(showDeleteUserById)}/>}
+            {showAddUser && <UserAdd onClose={addUserCloseHandler} onSave={addUserSaveHandler} />}
+
+            {showUserById && <UserDetails user={users.find(user => user._id == showUserById)} onClose={() => setShowUserById(null)} />}
+
+            {showDeleteUserById && <UserDelete onClose={userDeleteCloseHandler} onUserDelete={() => userDeleteHandler(showDeleteUserById)} />}
 
             <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
-            
-            <Pagination/>
+
+            <Pagination />
         </section>
     )
 }
